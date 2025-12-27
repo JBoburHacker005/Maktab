dd38b314597407e86e12f767bcb6d20115590394
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2025-12-27 13:11:27 +0500

    zo'r

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -177,7 +177,7 @@ const About: React.FC = () => {
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: 'url(/maktab/photo_2025-12-13_13-23-10.jpg)',
+            backgroundImage: 'url(/maktab/rasm.png)',
           }}
         />
         <div className="absolute inset-0 bg-background/60 backdrop-blur-md" />
