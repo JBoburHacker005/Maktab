5371052e8f7d7a5f02ff2552d690831a1927be03
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2026-01-23 17:54:32 +0500

    zo'r

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -267,6 +267,7 @@ const News: React.FC = () => {
                         trimmed.startsWith('🌍') ||
                         trimmed.startsWith('⚡️') ||
                         trimmed.startsWith('🎯') ||
+                        trimmed.startsWith('🌸') ||
                         trimmed.startsWith('📸') ||
                         trimmed.startsWith('🗓') ||
                         trimmed.startsWith('🏢') ||
